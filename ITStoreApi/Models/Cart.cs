e90745405c3ae4f78a08fff63f1840986e5e2960
kittiphop <kittiphopzheng@hotmail.com>
2023-03-28 23:34:04 +0700

    add product amount property

--- ITStoreApi/Models/Cart.cs
+++ ITStoreApi/Models/Cart.cs
@@ -12,5 +12,6 @@ namespace ITStoreApi.Models
         public string ProductImg { get; set; }
         public string ProductName { get; set; }
         public float ProductPrice { get; set; }
+        public int ProductAmount { get; set; }
     }
 }
