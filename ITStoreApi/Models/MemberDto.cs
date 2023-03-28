fd902c1a1484d8dcc2e03d054a087e0cc2fdcf05
kittiphop <kittiphopzheng@hotmail.com>
2023-03-28 23:34:26 +0700

    change dto model

--- ITStoreApi/Models/MemberDto.cs
+++ ITStoreApi/Models/MemberDto.cs
@@ -2,11 +2,7 @@
 {
     public class MemberDto
     {
-        public int MemberId { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Role { get; set; }
     }
 }
